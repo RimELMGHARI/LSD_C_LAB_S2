be2003e417f8da064082cf9c259cc56f1707ffdc
RimELMGHARI <73057243+RimELMGHARI@users.noreply.github.com>
2021-03-21 21:53:45 +0100

    Update iterative_fibo.c

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -2,5 +2,29 @@
 
 int iterative_fibo(int n)
 {
-    // write your iterative_fibo code here
+    if (n==0)
+    {
+        return 0;
+    }
+
+    if (n==1)
+    {
+        return 1;
+    }
+
+    int f0=0;
+    int f1=1;
+    int fi;
+    int i;
+    if (n>=2)
+    {
+        for (i==2;i<=n;i++)
+        {
+          fi=f0+ f1;
+          f0=f1;
+          f1=fi ;
+        }
+    }
+
+    return fi;
 }
