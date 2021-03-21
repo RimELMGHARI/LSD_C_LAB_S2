9ee4ce9b93e0c7b8d2a20c48d4043cea1f6e363e
RimELMGHARI <73057243+RimELMGHARI@users.noreply.github.com>
2021-03-21 22:27:43 +0100

    Update print_table.c

--- TP1/EX2/print_table.c
+++ TP1/EX2/print_table.c
@@ -1,9 +1,14 @@
 #include "header.h"
 
 /*
-** Note: iterating pointer array is the same as stack array.
+** Note: iterating pointer array is the same as stack array.ok
 */
 void    print_table(int *tab, int size)
 {
     // iterate the array tab and print its elements
-}
+    int i;
+    for(i=0; i<size; i++)
+    {
+    printf("T[%d] = %d \t",i,T[i]);
+    }
+}    
