2f7cba264f93c4e6fda9a151daf637743c82857d
RimELMGHARI <73057243+RimELMGHARI@users.noreply.github.com>
2021-03-21 11:31:51 +0100

    Update recursive_fibo.c

--- TP1/EX1/recursive_fibo.c
+++ TP1/EX1/recursive_fibo.c
@@ -1,6 +1,29 @@
 #include "header.h"
 
+
 int recursive_fibo(int n)
 {
-    // write your recursive fibo code here
+    if (n==0)
+    {
+        return 0;
+    }
+
+    if (n==1)
+    {
+        return 1;
+    }
+
+    return(recursive_fibo(n-2)+ recursive_fibo(n-1));
+}
+
+
+int main()
+{
+    int n;
+    printf("enter n:\n");
+    scanf("%d",&n);
+    printf("fib(%d)=%d",n ,recursive_fibo(n));
+    return 0;
+}
+
 }
