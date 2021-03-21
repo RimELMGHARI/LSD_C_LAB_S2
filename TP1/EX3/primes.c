3861fa7c0aa44743421c96f14abecd4632269953
RimELMGHARI <73057243+RimELMGHARI@users.noreply.github.com>
2021-03-21 23:26:32 +0100

    Update primes.c

--- TP1/EX3/primes.c
+++ TP1/EX3/primes.c
@@ -8,12 +8,27 @@
 
 static bool    is_prime(int n)
 {
-    // write is_prime function code here
+    int i;
+    for (i==2; i< abs(n); i++)
+    {
+        if(n%i==0)
+        { 
+            return 0;
+        }
+    }
+    return 1;
 }
 
 
 void    get_primes(int n)
 {
-    // write get_primes function code here that use is_prime function
+    int i;
+    for(i==2; i< abs(n); i++)
+    {
+        if (is_prime(i)==1
+            {
+                printf("%d",i);
+            }
+    }
 }
 
