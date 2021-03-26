232ae3fd067d163e8f0ba071d781f77dc8655487
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-03-26 19:38:50 +0100

    Update header.h

--- TP2/EX2/header.h
+++ TP2/EX2/header.h
@@ -1,1 +1,21 @@
+#ifndef HEADER
+#define HEADER
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int struct node
+{
+    int value;
+    struct node *next_node;
+}
+typedef struct node node;
+typedef struct node *t_list;
+
+node create_node(node n);
+void add_node(node n); //add to the end of the list
+list array_to_list(array t);
+void print_list(list l);
+void remove_node(*node *p);
+
+#endif
