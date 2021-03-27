c5d48a94315b1f1c2396df5246dd50102bc7bc48
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-03-27 11:04:32 +0100

    Update array_to_list.c

--- TP2/EX2/array_to_list.c
+++ TP2/EX2/array_to_list.c
@@ -1,1 +1,17 @@
+#include "header.h"
 
+list array_to_list(int length);
+{
+    int T[length];
+    t_list L;
+
+    if(length==0)
+        return L;
+
+    L=create_node(T[0]);
+    for (int i = 1; i < length ; i++)
+    {
+        add_node(T[i],L);
+    }
+    return L;
+}
