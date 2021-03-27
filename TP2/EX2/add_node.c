71457236787e6bd7d0ccf190d24e20c9b6f704b7
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-03-27 11:03:50 +0100

    Update add_node.c

--- TP2/EX2/add_node.c
+++ TP2/EX2/add_node.c
@@ -1,1 +1,17 @@
+#include "header.h"
+
+void add_node(int x,t_list * L)
+{
+   t_list c=(node *)malloc(sizeof(node));
+   c.value= x;
+   c.next_node= NULL;
+    if(*L==NULL)
+        L = C;
+    else
+    {
+        liste t = *L;
+        while(t->next_node != NULL)
+            t = t->next_node;
+    }
+}
 
