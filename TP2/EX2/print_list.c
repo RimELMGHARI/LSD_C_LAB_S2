85fdc707fa9a33461bed1c843aef89e4193a07ff
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-03-27 11:05:32 +0100

    Update print_list.c

--- TP2/EX2/print_list.c
+++ TP2/EX2/print_list.c
@@ -1,1 +1,15 @@
+#include "header.h"
 
+void print_list(t_list l);
+{
+    t_list t=L;
+    printf("[");
+    while(t.next_node!=NULL)
+    {
+        printf("%d ",t.value);
+        t=t.next_node;
+    }
+    printf("%d]",t.value);
+
+
+}
