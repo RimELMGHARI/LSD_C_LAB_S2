088725f4749ba5aef0d309ce39157c92b4c6d967
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-03-27 11:11:59 +0100

    Update main.c

--- TP2/EX2/main.c
+++ TP2/EX2/main.c
@@ -1,1 +1,10 @@
-
+int main()
+{
+    node n;
+    create_node(n);
+    add_node(n); //add to the end of the list
+    array_to_list(n,lenght);
+    print_list(l);
+    remove_node(*p, T );
+    return 0;
+}
