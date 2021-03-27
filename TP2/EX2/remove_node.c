8e1f420a12343b9ba8eb6f7dfc0168c3ceea7c7f
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-03-27 11:06:22 +0100

    Update remove_node.c

--- TP2/EX2/remove_node.c
+++ TP2/EX2/remove_node.c
@@ -1,1 +1,27 @@
+#include "header.h"
+
+void remove_node(node *p; t_list *t);
+{
+    while (*t.next_node !=p)
+    {
+        *t=*t.next_node;
+        if  (*t.next_node== p)
+        {
+            free(*t);
+            *t=
+        }
+        else
+        {
+            liste l = *t;
+            liste l1 = *t;
+            while(l.next_node != p)
+                {
+                l1 = l;
+                l = l.next_node;
+                }
+            l1.next_node = l;
+            free(l);
+        }
+    }
+}
 
