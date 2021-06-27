e45bdb443245753ec66d0cf3f6d3fde02dd04a0a
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-06-27 12:09:21 +0200

    Update main.c

--- TP3/EX1/main.c
+++ TP3/EX1/main.c
@@ -1,1 +1,18 @@
 
+
+
+int main(int ac, char **av)
+{
+	printf("%d\n", ac);
+	int i=0,j=0;
+	while(i<ac)
+	{
+		while (av[i][j] !="\0")
+		{
+			printf("%c\n", av[i][j]);
+			j++;
+		}
+		i++
+	}
+	return 0;	
+}
