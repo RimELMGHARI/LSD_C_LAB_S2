f60d6fdb5b0d3b79069cb3afa76fb83eb1517a05
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-06-27 12:20:31 +0200

    Update ex1.c

--- TP4/ex1.c
+++ TP4/ex1.c
@@ -1,1 +1,29 @@
+include "header.h"
+  
 
+int leaf_sum(tree T){
+    if (T==NULL)
+        return 0;
+    return 1+leaf_sum(T)
+}
+
+#include "header.h"
+int tree_depth(tree T){
+    if (T==NULL)
+        return 0;
+    if (T->left==NULL && T->right == NULL)
+        return 1;
+    if ( T->left==NULL)
+        return 1+tree_depth(T->right);
+    if (T->right==NULL)
+        return 1+tree_depth(T->left);
+    return max(tree_depth(T->left),tree_depth(T->right));
+}
+
+int tree_diameter(tree T){
+    if (T==NULL)
+        return 0;
+    if (T->left==NULL && T->right == NULL)
+        return 1;
+    return 1+tree_depth(T->left)+tree_depth(T->right);
+}
