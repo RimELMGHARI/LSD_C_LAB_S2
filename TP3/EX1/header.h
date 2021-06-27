82f3e11c165a17488b30ef738d44178851f71213
rim <73057243+RimELMGHARI@users.noreply.github.com>
2021-06-27 12:02:38 +0200

    Update header.h

--- TP3/EX1/header.h
+++ TP3/EX1/header.h
@@ -1,1 +1,26 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+struct celluleDouble
+{
+    char element;
+    struct celluleDouble * suivant;
+    struct celluleDouble * precedent;
+};
+typedef struct celluleDouble celluleDouble;
+
+struct listeDouble
+{
+    celluleDouble * debut;
+    celluleDouble * fin;
+    unsigned longueur;
+};
+typedef struct listeDouble listeDouble;
+
+//prototypes
+void ajoutFinD(char ,listeDouble *);
+void affichageListeDouble(listeDouble );
+int listeDoubleVide(listeDouble );
